139031e825ed5b65d8b2f6c5b1e22a6d105ac953
Deivys Figueroa <deivysfigueroasantacruz@gmail.com>
2022-09-27 23:36:18 -0500

    modified

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
             <BrowserRouter basename="/react-ecomerce">
                 <Layout>
                     <Routes>
-                        <Route path="/react-ecomerce/" element={<Home />} />
+                        <Route path="/" element={<Home />} />
                         <Route path="login" element={<Login />} />
                         <Route path="account" element={<MyAccount />} />
                         <Route path="Checkout" element={<Checkout />} />
